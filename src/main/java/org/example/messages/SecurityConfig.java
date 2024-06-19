177e123e709d1b9db51ee79c0aada1400d8f6b06
Rob Winch <rwinch@users.noreply.github.com>
2024-06-19 16:30:21 -0500

    UserDetailsService

--- src/main/java/org/example/messages/SecurityConfig.java
+++ src/main/java/org/example/messages/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.WebauthnConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @EnableWebSecurity
@@ -32,5 +35,20 @@ public class SecurityConfig {
 				.permitAll()
 			);
 		return http.build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails rob = User.withDefaultPasswordEncoder()
+				.username("rob")
+				.password("password")
+				.roles("USER")
+				.build();
+		UserDetails josh = User.withDefaultPasswordEncoder()
+				.username("josh")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(rob, josh);
 	}
 }
