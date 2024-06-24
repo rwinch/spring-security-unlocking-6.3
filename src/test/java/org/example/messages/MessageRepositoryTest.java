284518cdc09a9c91fef77989ee3279065aa4f589
Rob Winch <rwinch@users.noreply.github.com>
2024-06-24 10:54:17 -0500

    Test Eve

--- src/test/java/org/example/messages/MessageRepositoryTest.java
+++ src/test/java/org/example/messages/MessageRepositoryTest.java
@@ -3,8 +3,10 @@ package org.example.messages;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @SpringBootTest
 public class MessageRepositoryTest {
@@ -16,5 +18,12 @@ public class MessageRepositoryTest {
 		assertThat(messages.findById(100L).get())
 				.usingRecursiveComparison()
 				.isEqualTo(new Message(100L, "Hi Josh", "This message is from Rob", rob, josh));
+	}
+
+	@Test
+	@WithEveUser
+	void eve(@Autowired MessageRepository messages) {
+		assertThatExceptionOfType(AuthorizationDeniedException.class)
+				.isThrownBy(() -> messages.findById(100L));
 	}
 }
