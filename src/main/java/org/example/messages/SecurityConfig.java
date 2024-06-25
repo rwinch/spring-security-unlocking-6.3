1c4008fada5c3d1d651ffe93cfeb716696a5306a
Rob Winch <rwinch@users.noreply.github.com>
2024-06-25 16:25:22 -0500

    add passkeys

--- src/main/java/org/example/messages/SecurityConfig.java
+++ src/main/java/org/example/messages/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.WebauthnConfigurer;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @EnableWebSecurity
@@ -18,6 +19,11 @@ public class SecurityConfig {
 				.anyRequest().authenticated()
 			)
 			.httpBasic(Customizer.withDefaults())
+			.with(new WebauthnConfigurer<>(), (passkeys) -> passkeys
+				.rpName("Spring Security Relying Party")
+				.rpId("example.localhost")
+				.allowedOrigins("https://example.localhost:8443")
+			)
 			.formLogin(Customizer.withDefaults());
 		return http.build();
 	}
